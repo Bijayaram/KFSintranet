e31785c04d0ce91a92724024ad06415243cf716a
Bijayaram <47294095+Bijayaram@users.noreply.github.com>
2022-07-24 22:13:34 +0900

    added Cost Estimate, Script

---
+++ script.js
@@ -0,0 +1,25 @@
+$(".estimate").click(function(){
+  var fee = (parseFloat($("#course-fee").val()))/2;
+  var exchangeRate = parseFloat($(".exchange-rate").val());
+  var scholarship = parseFloat($(".scholarship").val());
+  var medicalAUD = 1500;
+  var flightCost = 2000;
+  var immiAUD = 638;
+  var surAUD = 2000;
+  var totalCost = fee+exchangeRate+medicalAUD+flightCost+immiAUD+surAUD-scholarship;
+  $(".tuition-fee-AUD").html(fee);
+  $(".tuition-fee-NPR").html(fee*exchangeRate);
+  $(".scholarship-AUD").html(fee*(scholarship/100));
+  $(".scholarship-NPR").html(fee*(scholarship/100)*exchangeRate);
+  $(".medical-NPR").html(exchangeRate*medicalAUD);
+  $(".medical-AUD").html(medicalAUD);
+  $(".immi-AUD").html(immiAUD);
+  $(".immi-NPR").html(exchangeRate*immiAUD);
+  $(".sur-AUD").html(surAUD);
+  $(".sur-NPR").html(exchangeRate*surAUD);
+  $(".flight-AUD").html(flightCost);
+  $(".flight-NPR").html(exchangeRate*flightCost);
+  $(".total-AUD").html(totalCost);
+  $(".total-NPR").html(totalCost*exchangeRate);
+});
+  // alert("I am here");
