046c56c04260ecd673166529727390e85584cb33
Bijayaram <47294095+Bijayaram@users.noreply.github.com>
2023-02-12 09:25:18 +0900

    Added pop up box

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
-$(".estimate").click(function(){
-  var fee = (parseFloat($("#course-fee").val()))/2;
+$(".estimate").click(function () {
+  var fee = parseFloat($("#course-fee").val()) / 2;
   var exchangeRate = parseFloat($(".exchange-rate").val());
-  var scholarship = parseFloat($(".scholarship").val())*-1;
+  var scholarship = parseFloat($(".scholarship").val()) * -1;
   var medicalAUD = 1500;
   var flightCost = 2000;
   var immiAUD = 638;
   var surAUD = 2000;
-  var totalCost = fee+exchangeRate+medicalAUD+flightCost+immiAUD+surAUD-scholarship;
+  var totalCost =
+    fee +
+    exchangeRate +
+    medicalAUD +
+    flightCost +
+    immiAUD +
+    surAUD -
+    scholarship;
   $(".tuition-fee-AUD").html(fee);
-  $(".tuition-fee-NPR").html(fee*exchangeRate);
-  $(".scholarship-AUD").html(fee*(scholarship/100));
-  $(".scholarship-NPR").html(fee*(scholarship/100)*exchangeRate);
-  $(".medical-NPR").html(exchangeRate*medicalAUD);
+  $(".tuition-fee-NPR").html(fee * exchangeRate);
+  $(".scholarship-AUD").html(fee * (scholarship / 100));
+  $(".scholarship-NPR").html(fee * (scholarship / 100) * exchangeRate);
+  $(".medical-NPR").html(exchangeRate * medicalAUD);
   $(".medical-AUD").html(medicalAUD);
   $(".immi-AUD").html(immiAUD);
-  $(".immi-NPR").html(exchangeRate*immiAUD);
+  $(".immi-NPR").html(exchangeRate * immiAUD);
   $(".sur-AUD").html(surAUD);
-  $(".sur-NPR").html(exchangeRate*surAUD);
+  $(".sur-NPR").html(exchangeRate * surAUD);
   $(".flight-AUD").html(flightCost);
-  $(".flight-NPR").html(exchangeRate*flightCost);
+  $(".flight-NPR").html(exchangeRate * flightCost);
   $(".total-AUD").html(totalCost);
-  $(".total-NPR").html(totalCost*exchangeRate);
+  $(".total-NPR").html(totalCost * exchangeRate);
 });
-  // alert("I am here");
+document.querySelector(".AustraliaPC").addEventListener("click", function () {
+  document.querySelector(".Australia-defination").classList.remove("hide");
+});
+document.querySelector(".closeButton").addEventListener("click", function () {
+  document.querySelector(".Australia-defination").classList.add("hide");
+});
+// alert("I am here");
